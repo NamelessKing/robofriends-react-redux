3cf6d7b84395b93d2318891353b4bea9dbbedccd
namelessking <habibur.tanjin@gmail.com>
2018-12-21 02:55:33 +0100

    added key property for all cards

--- src/CardList.js
+++ src/CardList.js
@@ -2,9 +2,18 @@ import React from "react";
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+
     const cardComponent = robots.map((robot) => {
-        return <Card id={robot.id} name={robot.name} email={robot.email} />
+        return (
+            <Card
+                key={robot.id}
+                id={robot.id}
+                name={robot.name}
+                email={robot.email}
+            />
+        )
     })
+
     return (
         <div>
             {cardComponent}
